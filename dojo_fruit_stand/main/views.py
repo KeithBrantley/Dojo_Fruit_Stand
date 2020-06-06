abb189b4a4b36673b8ed4350c38b9f0cbf874492
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-06 12:43:11 -0700

    Checkout page was created

--- dojo_fruit_stand/main/views.py
+++ dojo_fruit_stand/main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Woo! We made a new route!')
+    return HttpResponse('This is actually fun!')
+
+def checkout(request):
+    return HttpResponse('This is the checkout page')
