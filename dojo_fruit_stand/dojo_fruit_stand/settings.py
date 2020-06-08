1e56438473ebc51d5491653dea82e574bcc41833
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-08 11:56:12 -0700

    FIXED THE CSS FINALLY!

--- dojo_fruit_stand/dojo_fruit_stand/settings.py
+++ dojo_fruit_stand/dojo_fruit_stand/settings.py
@@ -122,12 +122,12 @@ STATIC_URL = '/static/'
 # #################################################
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
-    #"django.contrib.staticfiles.finders.AppDirectoriesFinder"
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
 )
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    '/main/templates/static',
+    '/main/static',
 ) 
 ######################################################
 # STATICFILES_DIRS = [
